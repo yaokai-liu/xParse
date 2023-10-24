dadc2f180eb4396c28091380089f9d44fd7359d4
Yaokai Liu <yaokai.liu@foxmail.com>
2023-10-24 21:35:07 +0800

    add 'xparse'.

---
+++ xparse/xparse.h
@@ -0,0 +1,47 @@
+//
+// Project Name: xParse
+// Filename: xparse.h
+// Creator: Yaokai Liu
+// Create Date: 2023-10-23
+// Copyright (c) 2023 Yaokai Liu. All rights reserved.
+//
+
+#ifndef XPARSE_XPARSE_H
+#define XPARSE_XPARSE_H
+
+#include "mem_space.h"
+
+typedef struct {
+    mem_space * INST_SPACE[2];
+    mem_space * CHAR_SPACE;
+
+    char_t * err_pos;
+    xSize err_len;
+    xuInt used:1;
+    xuInt unused:1;
+} xParser;
+
+#ifdef XPARSE_DEBUG
+#define trace_enter() do { \
+    fprintf(trace_out, "%sEnter <%s>, sp = %ld;\n", &trace_indent[0], __FUNCTION__, sp - cur_text); \
+    trace_indent.push(xReChar('-')); \
+} while(false)
+
+#define trace_leave() do { \
+    trace_indent.pop(nullptr); \
+    fprintf(trace_out, "%sLeave <%s>, sp = %ld;\n", &trace_indent[0], __FUNCTION__, sp - cur_text); \
+} while(0)
+#else
+#define trace_enter() do {0;} while(0)
+#define trace_leave() do {0;} while(0)
+#endif
+
+#define check(expr, expect_len) do { \
+    if (!(expr)) { \
+        parser->err_pos = sp; \
+        parser->err_len = expect_len; \
+        return 0; \
+    } \
+} while (false)
+
+#endif //XPARSE_XPARSE_H
