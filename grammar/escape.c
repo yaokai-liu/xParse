b9e0a61414f17ca2991e12b48aba2bb6edfa2266
Yaokai Liu <yaokai.liu@foxmail.com>
2023-10-24 21:40:30 +0800

    add escape symbols.

---
+++ grammar/escape.c
@@ -0,0 +1,16 @@
+//
+// Project Name: xParse
+// Filename: escape.c
+// Creator: Yaokai Liu
+// Create Date: 2023-10-23
+// Copyright (c) 2023 Yaokai Liu. All rights reserved.
+//
+
+#include "escape.h"
+
+#define CONTROL_SYM "()[]{}<>\\|@#$&~:;=+-*/?!^."
+#define BLANK_SYM "\n\r\v\f\t "
+
+static const char_t * const NON_PLAIN = string_t(BLANK_SYM CONTROL_SYM "\0");
+static const char_t * const SINGLE_ESCAPE = string_t("nrtfv " CONTROL_SYM "\0");
+static const char_t * const CLASS_ESCAPE = string_t("sSwWdDcC" "\0");
---
+++ grammar/escape.h
@@ -0,0 +1,18 @@
+//
+// Project Name: xParse
+// Filename: escape.h
+// Creator: Yaokai Liu
+// Create Date: 2023-10-23
+// Copyright (c) 2023 Yaokai Liu. All rights reserved.
+//
+
+#ifndef XPARSE_ESCAPE_H
+#define XPARSE_ESCAPE_H
+
+#include "char_t.h"
+
+static const char_t * const NON_PLAIN;
+static const char_t * const SINGLE_ESCAPE;
+static const char_t * const CLASS_ESCAPE;
+
+#endif //XPARSE_ESCAPE_H
